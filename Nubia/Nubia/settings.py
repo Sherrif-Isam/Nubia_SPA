320c51c354849edaad8ce4d9c1d82a056b8b5282
Sherrif Isam <sherrifisam@yahoo.co.uk>
2024-03-08 22:31:20 +0000

    Extended base to parts

--- Nubia/Nubia/settings.py
+++ Nubia/Nubia/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
      'SPA',
+     'Nubia'
 ]
 
 MIDDLEWARE = [
@@ -117,18 +118,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Static files (CSS, Images)
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'Nubia/static'),
 ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- Nubia/SPA/views.py
+++ Nubia/SPA/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def demo(request):
-    return render(request, 'demo.html')
+    return render(request, 'base.html')
