175c672e1e4888bbb181e26f3ac689bfe00e2b66
Sherrif Isam <sherrifisam@yahoo.co.uk>
2024-03-08 21:10:22 +0000

    created a static folder and collected static

--- Nubia/Nubia/settings.py
+++ Nubia/Nubia/settings.py
@@ -119,6 +119,16 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Static files (CSS, Images)
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'Nubia/static'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
